69c1aedfb4e7a7f072e4300027eef3ec6c3ee32a
Yoonje Choi <yoonjechoi@gmail.com>
2017-11-11 13:00:26 +0900

    Initial commit

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return (
+            <h1>Hello, This is a Moive.</h1>
+        );
+    }
+}
+
+export default Movie;
