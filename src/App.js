71248e444456df8b9fb9e8bc0a9e4707bf0b0b66
Yoonje Choi <yoonjechoi@gmail.com>
2017-11-11 19:17:29 +0900

    feat(Movie): use state of component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://metrouk2.files.wordpress.com/2017/02/the-matrix.jpg?quality=80&strip=all"
+  },
+  {
+    title: "Hunger Game",
+    poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAyMjQ3OTAxMzNeQTJeQWpwZ15BbWU4MDU0NzA1MzAx._V1_UY1200_CR90,0,630,1200_AL_.jpg"
+  }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  // movies: [...prevState.movies,
+  //   {
+  //     title: '도깨비',
+  //     poster: "http://img.lifestyler.co.kr/uploads/program/cheditor/2016/12/SN9HGA4BTLTE2CD7PC4J_1024x0.jpg"
+  //   }]
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((prevState, props) => ({
+        movies: [
+          ...movies,
+          {
+            title: '도깨비',
+            poster: "http://img.lifestyler.co.kr/uploads/program/cheditor/2016/12/SN9HGA4BTLTE2CD7PC4J_1024x0.jpg"
+          }]
+      }));
+    }, 5000);
+  }
+
+  _render_movies = () => {
+    const movies = this.state.movies.map((movie, index, array) => {
+      return <Movie title={movie.title} poster={movie.poster} />;
+    });
+
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        <Movie/>
-        <Movie/>
-        <Movie/>
-        <Movie/>
-        <Movie/>
-        
+        {this.state.movies ? this._render_movies() : <h1>Loading</h1>}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
     render() {
         return (
-            <h1>Hello, This is a Moive.</h1>
+            <div>
+                <h1>{this.props.title}</h1>
+                <MoviePoster poster={this.props.poster}/>
+            </div>
+
+        );
+    }
+}
+
+class MoviePoster extends Component {
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+    
+    render() {
+        return (
+            <img src={this.props.poster} />
         );
     }
 }
